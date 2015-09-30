2065955e03fa26dc98d3803eb4cc2ba51937ad14
Svetoslav Stanev <svstanev@gmail.com>
2015-09-30 11:38:07 +0300

    Fixed dependency file name

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports.Route = require('./src/route').Route;
-
 module.exports.express = require('./src/express').middleware;
 module.exports.express.Result = require('./src/express').Result;
 
